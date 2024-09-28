bdc13abdf5670ff0dcc028784032bad459fa1ba2
Muhammad Muzzammil <muhammadmuzzammil868@gmail.com>
2024-09-29 03:23:32 +0500

    Javascript Currency Converter Project With API

---
+++ script.js
@@ -0,0 +1,238 @@
+let URL = `https://v6.exchangerate-api.com/v6/bf4b8b1261dcc591db2a3ed6/latest`;
+
+const dropdownSelect = document.querySelectorAll('select');
+const bttn = document.querySelector('button');
+const input = document.querySelector('input');
+const fromCurrency = document.querySelector('.fromCurrency select');
+const toCurrency = document.querySelector('.toCurrency select')
+const finalMessage = document.querySelector('.message p')
+
+
+
+
+const countryList = {
+    AED: "AE",
+    AFN: "AF",
+    XCD: "AG",
+    ALL: "AL",
+    AMD: "AM",
+    ANG: "AN",
+    AOA: "AO",
+    AQD: "AQ",
+    ARS: "AR",
+    AUD: "AU",
+    AZN: "AZ",
+    BAM: "BA",
+    BBD: "BB",
+    BDT: "BD",
+    XOF: "BE",
+    BGN: "BG",
+    BHD: "BH",
+    BIF: "BI",
+    BMD: "BM",
+    BND: "BN",
+    BOB: "BO",
+    BRL: "BR",
+    BSD: "BS",
+    NOK: "BV",
+    BWP: "BW",
+    BYR: "BY",
+    BZD: "BZ",
+    CAD: "CA",
+    CDF: "CD",
+    XAF: "CF",
+    CHF: "CH",
+    CLP: "CL",
+    CNY: "CN",
+    COP: "CO",
+    CRC: "CR",
+    CUP: "CU",
+    CVE: "CV",
+    CYP: "CY",
+    CZK: "CZ",
+    DJF: "DJ",
+    DKK: "DK",
+    DOP: "DO",
+    DZD: "DZ",
+    ECS: "EC",
+    EEK: "EE",
+    EGP: "EG",
+    ETB: "ET",
+    EUR: "FR",
+    FJD: "FJ",
+    FKP: "FK",
+    GBP: "GB",
+    GEL: "GE",
+    GGP: "GG",
+    GHS: "GH",
+    GIP: "GI",
+    GMD: "GM",
+    GNF: "GN",
+    GTQ: "GT",
+    GYD: "GY",
+    HKD: "HK",
+    HNL: "HN",
+    HRK: "HR",
+    HTG: "HT",
+    HUF: "HU",
+    IDR: "ID",
+    ILS: "IL",
+    INR: "IN",
+    IQD: "IQ",
+    IRR: "IR",
+    ISK: "IS",
+    JMD: "JM",
+    JOD: "JO",
+    JPY: "JP",
+    KES: "KE",
+    KGS: "KG",
+    KHR: "KH",
+    KMF: "KM",
+    KPW: "KP",
+    KRW: "KR",
+    KWD: "KW",
+    KYD: "KY",
+    KZT: "KZ",
+    LAK: "LA",
+    LBP: "LB",
+    LKR: "LK",
+    LRD: "LR",
+    LSL: "LS",
+    LTL: "LT",
+    LVL: "LV",
+    LYD: "LY",
+    MAD: "MA",
+    MDL: "MD",
+    MGA: "MG",
+    MKD: "MK",
+    MMK: "MM",
+    MNT: "MN",
+    MOP: "MO",
+    MRO: "MR",
+    MTL: "MT",
+    MUR: "MU",
+    MVR: "MV",
+    MWK: "MW",
+    MXN: "MX",
+    MYR: "MY",
+    MZN: "MZ",
+    NAD: "NA",
+    XPF: "NC",
+    NGN: "NG",
+    NIO: "NI",
+    NPR: "NP",
+    NZD: "NZ",
+    OMR: "OM",
+    PAB: "PA",
+    PEN: "PE",
+    PGK: "PG",
+    PHP: "PH",
+    PKR: "PK",
+    PLN: "PL",
+    PYG: "PY",
+    QAR: "QA",
+    RON: "RO",
+    RSD: "RS",
+    RUB: "RU",
+    RWF: "RW",
+    SAR: "SA",
+    SBD: "SB",
+    SCR: "SC",
+    SDG: "SD",
+    SEK: "SE",
+    SGD: "SG",
+    SKK: "SK",
+    SLL: "SL",
+    SOS: "SO",
+    SRD: "SR",
+    STD: "ST",
+    SVC: "SV",
+    SYP: "SY",
+    SZL: "SZ",
+    THB: "TH",
+    TJS: "TJ",
+    TMT: "TM",
+    TND: "TN",
+    TOP: "TO",
+    TRY: "TR",
+    TTD: "TT",
+    TWD: "TW",
+    TZS: "TZ",
+    UAH: "UA",
+    UGX: "UG",
+    USD: "US",
+    UYU: "UY",
+    UZS: "UZ",
+    VEF: "VE",
+    VND: "VN",
+    VUV: "VU",
+    YER: "YE",
+    ZAR: "ZA",
+    ZMK: "ZM",
+    ZWD: "ZW",
+};
+
+for (let select of dropdownSelect) {
+    for (let code in countryList) {
+        let newOptions = document.createElement('option');
+        newOptions.value = code;
+        newOptions.innerText = code;
+        if (select.name === 'From' && code === 'USD') {
+            newOptions.selected = true;
+        } else if (select.name === 'To' && code === 'PKR') {
+            newOptions.selected = true;
+        }
+        select.append(newOptions);
+    };
+
+    select.addEventListener('change', (evt) => {     
+        changeFlag(evt.target);
+    });
+};
+
+
+const changeFlag = (element) => {
+    let currencyCode = element.value;
+    let countryCode = countryList[currencyCode];
+    let newSrc = `https://flagsapi.com/${countryCode}/flat/64.png`;
+    let images = element.parentElement.querySelectorAll('img');
+    images.forEach(image => {
+        image.src = newSrc;
+    });
+};
+
+bttn.addEventListener ('click', () => {
+   bttn.style.backgroundColor = 'purple';
+   setTimeout(() => {
+    bttn.style.backgroundColor = 'rgb(69, 1, 69)';
+   }, 500);
+   let inputVal = input.value
+   if (inputVal == '' || inputVal <= 0){
+    input.value = 1;
+    inputVal = input.value;
+   }
+
+    APIFunction(inputVal);
+});
+
+
+const APIFunction = async(inputVal) => {
+    let baseURL = `${URL}/${fromCurrency.value}`;
+    let response = await fetch(baseURL);
+    let responseJson = await response.json();
+    let data = responseJson.conversion_rates[toCurrency.value];
+    console.log(data);
+    messageFunction(data, inputVal);    
+}
+
+
+const messageFunction = (message, inputVal) => {
+        const totalVal = Number(inputVal);
+        finalMessage.innerText = `${totalVal}${fromCurrency.value} = ${totalVal * message}${toCurrency.value}`
+        
+}
+
+
+
+
+
